7400ca4852e04b20831e9e38b9f5c3a4525d3f0d
warramma <wishmorethewitch@gmail.com>
2024-07-29 16:14:48 -0500

    create operate function to take operator and two numbers

--- script.js
+++ script.js
@@ -15,4 +15,19 @@ function divide(num1, num2){
 
 let num1, num2, operator;
 
-
+function operate(operator, num1, num2){
+    switch(operator.toLowerCase()){
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case 'x':
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return 0;
+        
+    }
+}
