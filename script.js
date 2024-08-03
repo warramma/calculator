769e2b1fca11c88344f4bca92e2f6427d1f8444f
warramma <wishmorethewitch@gmail.com>
2024-08-03 11:33:38 -0500

    add variables for number buttons and clear

--- script.js
+++ script.js
@@ -31,3 +31,17 @@ function operate(operator, num1, num2){
         
     }
 }
+const display = document.querySelector(".display");
+const one = document.querySelector('#1');
+const two = document.querySelector('#2');
+const three = document.querySelector('#3');
+const four = document.querySelector('#4');
+const five = document.querySelector('#5');
+const six = document.querySelector('#6');
+const seven = document.querySelector('#7');
+const eight = document.querySelector('#8');
+const nine = document.querySelector('#9');
+const zero = document.querySelector('#0');
+const period = document.querySelector('#period');
+const negative = document.querySelector('#negative');
+const clear = document.querySelector('#clear');
